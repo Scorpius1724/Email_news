af92a457b7ce2ebca37596538a33526484afcb3b
Swapnil <swapnilacharjee1724@gmail.com>
2024-04-28 05:25:06 +0530

    Day 27 - Access the news titles and descriptions

---
+++ README.md
@@ -0,0 +1,2 @@
+# What is this project?
+This application accesses news articles regarding a specific topic/subject and sends them through email.
--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import requests
 
 
+# Store the API key and URL Link
 api_key = "92c33adb87f44865aa0bc5c8772cb6e4"
 url = "https://newsapi.org/v2/everything?q=tesla" \
       "&from=2024-03-27&sortBy=publishedAt&apiKey=92" \
       "c33adb87f44865aa0bc5c8772cb6e4"
+
+# Make request
 request = requests.get(url)
-content = request.text
-print(content)
+
+# Get a dictionary with data
+content = request.json()
+
+# Access the article titles and descriptions
+for article in content["articles"]:
+      print(article["title"])
+      print(article["description"])

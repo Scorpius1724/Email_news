73f0a1e2e246034ce90c0449321444e7ba0540dd
Swapnil <swapnilacharjee1724@gmail.com>
2024-05-01 19:43:13 +0530

    Day 28 - Control catagory of the news articles sent.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import requests
 from send_email import send_email
 
+topic = "tesla"
 # Store the API key and URL Link
 api_key = "92c33adb87f44865aa0bc5c8772cb6e4"
 url = "https://newsapi.org/v2/everything?" \
-      "q=tesla&" \
+      f"q={topic}&" \
       "sortBy=publishedAt&" \
       "apiKey=92c33adb87f44865aa0bc5c8772cb6e4&" \
       "language=en"
